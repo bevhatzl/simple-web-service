faddc54dec9cbb67b67f878f54e897c677d45af6
bevhatzl <beverleyhatzl@gmail.com>
2022-09-16 14:59:15 +1000

    change folder structure

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes/main.js';
+import router from './src/routes/routes.js';
 
 const app = express();
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { showsList } from './default.js';
+import { showsList } from '../controllers/showsList.js';
 
 const router = Router();
 
